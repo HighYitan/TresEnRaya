d4f8a33d2c4da26a94bf24e272feaee702cee46c
HighYitan <32370248+HighYitan@users.noreply.github.com>
2018-03-26 14:57:11 +0200

    Add files via upload

--- tresenraya/IA1.java
+++ tresenraya/IA1.java
@@ -1,34 +1,43 @@
 package tresenraya;
 
 public class IA1 extends IA0 {
-    private int peso[][];
+    int peso[][];
     public IA1(){
-        peso = new int[][]{{3,2,3}, {2,4,2}, {3,2,3},};
+        peso = new int[][]{{3,2,3}, {2,4,2}, {3,2,3}}; 
     }
     @Override
     public Movimiento movimiento(Tablero tablero) {
-        
-        
         int[][] casillas = tablero.getCasillas();
         Movimiento jugada = new Movimiento();
         for (int i = 0; i < casillas.length; i++) {
             for (int j = 0; j < casillas[i].length; j++) {
-                if (tablero.getCasilla(i, j) == 0) {
-                    jugada.setRow(i);
-                    jugada.setCol(j);
-                    jugada.setJugador(this);
-                    jugada.setBlancas(false);
-                    return jugada;
+                
+                if (peso[i][j] == buscarPeso(tablero)) {
+                    
+                    if(tablero.getCasilla(i, j) == 0){
+                        jugada.setRow(i);
+                        jugada.setCol(j);
+                        jugada.setJugador(this);
+                        jugada.setBlancas(false);
+                        return jugada;
+                    }
                 }
-                //else {
-                //    System.out.println();
-                //    System.out.println("Error de la CPU al mover, mal programado amigo");
-                //}
             }
         }
         return null;
     }
-    public void asignarPeso(int [][] peso){
-        peso = Tablero.setCasillas(peso);
+    public int buscarPeso(Tablero tablero) {
+        peso = new int[][]{{3,2,3}, {2,4,2}, {3,2,3}};    
+        int[][] casillas = tablero.getCasillas();
+        int pesoMinimo;
+        pesoMinimo = peso[0][1];
+        for (int i = 0; i < casillas.length; i++) {
+            for (int j = 0; j < casillas[i].length; j++) {
+                if (peso[i][j] >= pesoMinimo && tablero.getCasilla(i, j) == 0) {
+                    pesoMinimo = peso[i][j];   
+                }
+            }
+        }
+       return pesoMinimo;
     }
 }
